dd653b4849503e72ad0e99d00ca35aaa2ce80688
bormat <bormat@MacBook-Pro-de-bormat.local>
2018-10-29 22:03:28 +0100

    multiple correctifs

--- beautiful.js
+++ beautiful.js
@@ -1,175 +1,180 @@
 Array.prototype.last = function() {
-	return this[this.length-1];
+    return this[this.length-1];
 };
-
-var off = false;
-var on = true;
-var limit = 80;
-
-$('textarea').width('500px')
-$('textarea').height('500px')
-$('textarea').val('')
+var conf = {
+	break_line: false,//doit on sauter une ligne après un {
+	limit: 80// doit on sauter une ligne après un certain nombre de caractères dans les commentaires
+}
+$('textarea').width('200px').height('200px')
 var tab=[]
-var addBalise = function(name, text,unshift){//regex
-	tab[unshift || 'push']({
-		balise : name,
-		text : text
-	})
-	return '[ref='+ tab.length + ']';
+var unindent = function(text){
+  return text.replace(/[ \t]/g,'')// remove existing indentation
 }
 
-var toProtect = 'ref';
-var toRegExp = function(s){
-	return new RegExp('((' + s.replace(/\s/g, ')|(' ) + '))','g');
-}
-var encode = function(s){
-	return s.replace(toRegExp(toProtect), '$1enc')
-		.replace(/\\./g, function(m){
-			return addBalise('backSlash',m);  
-		})
-		//.replace(/\\\//g,'backsSlashPlusSlash')//backslash
-		//.replace(/\\/g,'backslash')//backslash;
-}
-var decode = function(s){
-	var regex = toRegExp((encode(toProtect)));
-	s = s.replace(regex,function(m){
-		return m.slice(0,-3)//enleve $enc à la fin des mot toprotect 
-	})
-	/*.replace(/backsSlashPlusSlash/g, '\\/')//backsSlashPlusSlash
-	.replace(/backslash/g, '\\')//backslash;;*/
-	return s;
+var decode = function(line3, ind,only_protected){
+  return line3.replace(/\[ref=(\d+)\]/g,function(m,$1){
+      var tag = tab[$1]
+      if(only_protected){
+        return tag.text
+      }
+      return (tag instanceof Protected_tag) ? m :  tag.decode(ind)
+  })
 }
 
- var rec = function(f,s1){
- 	do{
-		s2 = s1;
-		s1 = f(s1);
-	}while(s1 != s2);
-	return s1;
- }
+class Tag{
+  constructor(o){
+    this.name = o.name
+    this.text = o.text
+    this.indent_str = ''
+  }
+  unindent(){
+    this.text = this.text.replace(/[ \t]/g,'')// remove existing indentation
+  }
 
- var toXML = function(s1){//comment/\\/g
-	s1 =  encode(s1)		
-		.replace(/\/\/(.*)/g,function(m,$1){
-			return addBalise('scom',$1);// single comment
-		})
-		.replace(/\/\*(.*?)\*\//gm,function(m,$1){
-			return addBalise('mcom',$1);/*multiple comment */
-		})
-		.replace(/\/.*?\/\w*/g, function(m){
-			return addBalise('regex',m);//regex
-		})
-		.replace(/(["']).*?\1/g, function(m){
-			return addBalise('quote',m);//quote
-		})
+  decode(){
+    return this.text
+  }
+}
+  class Protected_tag extends Tag{
+      decode(indent){
+        throw 'this is not supposed to happen'
+      }
+  }
+  class RegExp_tag extends Tag{
 
-		.replace(/^[\n\s\t]+/gm,'')//pas d'espace en début de ligne
-		.replace(/[ ]+/gm,' ')//multiple space
-	addBalise('main',s1,'unshift')
+  }
+
+
+  class Comment_tag extends Tag{
+      constructor(o){
+        super(...arguments)
+        this.is_multiple = o.others.is_multiple
+      }
+
+      decode(ind){
+          var is_multiple = this.is_multiple
+          var sep = '\n' + (ind || '');
+          var lines = this.text.split('\n').map(function(line,i){
+              if(i > 0){
+                line = unindent(line)
+              }
+              do{
+                  var lg = line.length
+                  line = line.replace(eval('/.{'+ conf.limit +'}/g'), function(m){//detection of long lines
+                      return m.replace(/(.*)(\w*)/,function(m,$1,$2){ // split long line in two before the last word
+                          var sep = is_multiple ? ($2 !== '*' ? '*' : '')  : '//'
+                          return $1 + sep + $2;
+                      })
+                  })
+              }while(line.length != lg)//continue until there is no more too long line, it can happen when the line is very long that we have to do it several time
+              if(i && is_multiple){
+                line = line.replace(/(^[\t\s]*)\*?/g,'$1*')// put a * at the start if not present and not the first line of commment
+              }
+              return line
+          })
+          return this.text = lines.join(sep)
+      }
+  }
+
+class Main_tag extends Tag{
+  decode(indent_str){
+      this.unindent()
+      var text = addIndent(this.text)
+      var texts = text.split('\n').map(function(line3){
+          var inds = line3.match(/^\t*/)//get the indentation of the line
+          var ind = (indent_str || '') + (inds && inds[0] || '')
+          return decode(line3,ind,false)
+      })
+      return this.text = decode(texts.join('\n'),'',true)
+  }
 }
 
+//push the Tag in an Array, and return a reference to the position of the tag in the array
+//if unshift has value 'unshift' then push at the start of the array instead of the end
+var addBalise = function(name, text, others){//regex
+    var _class = eval(name)
+    tab.push( new _class({
+        balise : name,
+        text : text,
+        others: others
+    }))
+    return '[ref='+ (tab.length - 1) + ']';
+}
+
+var rec = function(f,s1){
+    do{
+        s2 = s1;
+        s1 = f(s1);
+    }while(s1 != s2);
+    return s1;
+}
+var toXML = function(s1){//comment/\\/g
+    // s1 = encode(s1)
+    //protect string that use [ref=a number]
+    s1 = s1.replace(/\[ref=(\d+)\]/g,function(m){
+        return addBalise('Protected_tag',m);
+    })
+    .replace(/\\./g, function(m){
+        return addBalise('Protected_tag',m,{
+          type:'backSlash'
+        });
+    })
+    .replace(/\/\/(.*)/g,function(m,$1){
+        return addBalise('Comment_tag',m,{
+          is_multiple: false
+        });// single comment
+    })
+    .replace(/\/\*([\s\S]*?)\*\//gm,function(m,$1){
+        return addBalise('Comment_tag',m,{
+          is_multiple: true
+        });/*multiple comment */
+    })
+    .replace(/\/.*?\/\w*/g, function(m){
+        return addBalise('RegExp_tag',m,{
+          type: 'regex'
+        });//regex
+    })
+    .replace(/(["']).*?\1/g, function(m){
+        return addBalise('Protected_tag',m,{
+          type: 'quote'
+        });//quote
+    })
+    .replace(/^[\n\s\t]+/gm,'')//pas d'espace en début de ligne
+    .replace(/[ ]+/gm,' ')//multiple space
+    addBalise('Main_tag',s1)
+}
 var getInd = function(nb){
-	var s2 = "";
-	for(var i = nb;i--;){
-		s2 += '\t'
-	}
-	return s2;
+    var s2 = "";
+    for(var i = nb;i-->0;){
+        s2 += '\t'
+    }
+    return s2;
 }
-
 var count = function(tab){
-	return tab ? tab.length : 0;
+    return tab ? tab.length : 0;
+}
+var addIndent = function(s,ind=0){
+    var s1 = ""
+    s.split('\n').forEach(function(str){
+        ind -= count(str.match(/\}/g))
+        s1 += getInd(ind) + str + '\n';
+        ind += count(str.match(/\{/g)) ;
+    })
+    return s1;
 }
 
-var ind = 0;
-var addIndent = function(s){
-	var s1 = ""	
-	s.split('\n').forEach(function(str){
-		ind -= count(str.match(/\}/g))
-		s1 += getInd(ind) + str + '\n';
-		ind += count(str.match(/\{/g)) ;
-	})
-	return s1;
+var beautifull = function(s){
+    tab = [];
+    toXML(s);
+    var main = tab[tab.length - 1]
+		if(conf.break_line){
+			main.text = main.text
+				 .replace(/\{/g,'{\n')/** saut de ligne après { */
+			   .replace(/\}/g,'\n}')
+		}
+    main.decode()
+    return main.text
 }
-	var arr = [];
-	var cropCom = function(text,tagName){
-		lastLine = (arr.join('') + text).split('\n').last()
-		var lg = lastLine.length;
-		if(lg - limit > 0){
-			var ind = getInd(count(lastLine.match(/\t/g)));	
-			var sep = '\n'+ ind + (tagName == 'scom' ? '//' : '');
-			text = text.replace(eval('/.{'+ (text.length + limit - lg) +'}/'), function(m){
-				return m.replace(/(.*)(\w*)/,function(m,$1,$2){
-					return $1 + sep + $2;
-				})
-			})
-			arr.push(text);
-		}else{
-			arr.push(text);
-		}
-	}
-
-	/*var cropCode = function(text,tagName){
-		lastLine = (arr.join('') + text).split('\n').last()
-		var lg = lastLine.length;
-		if(lg - limit > 0){
-			var ind = getInd(count(lastLine.match(/\t/g)));	
-			var sep = '\n'+ ind
-			text = text.replace(eval('/.{'+ (text.length + limit - lg) +'}/'), function(m){
-				return m.replace(/(.*)(\w*)/,function(m,$1,$2){
-					return $1 + sep + $2;
-				})
-			})
-			arr.push(text);
-		}else{
-			arr.push(text);
-		}
-	}*/
-	var crop = function(text,tagName){
-		if(tagName == 'scom' || tagName == 'mcom'){
-			cropCom(text,tagName)
-		}else{
-			cropCom(text);
-		}
-	}
-	var beautifull = function(s){
-	tab = []; 
-	toXML(s);
-	ind = 0; 
-	tab.forEach(function(s,i){
-		s.text = decode(s.text)
-		if( s.balise == 'scom'){
-			s.text = '//' + s.text;
-		}else if( s.balise == 'mcom'){
-			s.text = '/*' + s.text + '*/';
-		}
-	})
-
-	tab[0].text = tab[0].text.replace(/\{/g,'{\n').replace(/\}/g,'\n}').replace(/^[\n\s\t]+/gm,'')//pas d'espace en début de ligne
-	s = tab[0].text	= addIndent(tab[0].text)
-	
-
-	arr = []
-	arrCode = s.split(/\[ref=(\d+)\]/g);
-	tab.forEach(function(b){
-		b.text = b.text.replace(/\[ref=(\d+)\]/g,function(m,$1){
-			return tab[$1].text;
-		})
-	})
-	arrCode.forEach(function(text,i){
-		if(i%2){
-			crop(tab[text].text,tab[text].balise);
-		}else{
-			crop(text,'code')
-		}
-	})
-	s = arr.join('')
-	/*s = s.replace(/\n(\t+)\..*(\n\1\t.*)+?\n\1[^\t]/gm, function(m){
-		return m.replace(/(\t+)/g,'$1\t');
-	})*/
-	s = s.replace(/\t/g,'    ');
-	return s;
-}
-
-
-
-
+$('#indent').click(function(){
+    $('textarea').val(beautifull($('textarea').val()))
+})
